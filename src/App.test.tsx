41792bfdc5dafaca4c2b02a2739bf1cfe98c8280
William McPherson <willmcpherson2@gmail.com>
2021-06-10 23:45:43 +1000

    Initialise repository

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders heading', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/tape/i);
+  expect(linkElement).toBeInTheDocument();
+});
